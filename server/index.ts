46ebed3fa8dbd6a94f9f79c2ea8ac735f3e8b0fc
James Vicuna <jvicu001@gmail.com>
2025-07-01 18:00:37 -0700

    uploadPdf route added

--- server/index.ts
+++ server/index.ts
@@ -2,17 +2,28 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
+import multer from "multer";
 import { Settings, TextNode, VectorStoreIndex } from "llamaindex";
 import { openai, OpenAIEmbedding } from "@llamaindex/openai";
 import { SimpleDirectoryReader } from "@llamaindex/readers/directory";
 
+//LANGCHAIN
 import { BufferMemory } from "langchain/memory";
-import { ChatOpenAI } from "@langchain/openai";
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
 import {
   ChatPromptTemplate,
   MessagesPlaceholder,
 } from "@langchain/core/prompts";
 import { RunnableSequence } from "@langchain/core/runnables";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { readFile } from "fs/promises";
+
+import pdfParse from "pdf-parse";
+
+import { client } from "./db/supbase";
+import { buffer } from "stream/consumers";
 
 dotenv.config();
 const app = express();
@@ -29,6 +40,8 @@ Settings.llm = openai({
 Settings.embedModel = new OpenAIEmbedding();
 
 // langchain chatai settings
+const embeddings = new OpenAIEmbeddings({});
+
 const memory = new BufferMemory({
   memoryKey: "chat_history",
   returnMessages: true,
@@ -36,6 +49,37 @@ const memory = new BufferMemory({
 const chatModel = new ChatOpenAI({
   apiKey: process.env.OPENAI_API_KEY,
   model: "gpt-4o",
+});
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.post("/uploadPdf", upload.single("pdf"), async (req, res) => {
+
+  try {
+    const fileBuffer = req.file?.buffer;
+
+    if (!fileBuffer) {
+      res.status(400).json({ error: "No file uploaded" });
+      return;
+    }
+
+    const data = await pdfParse(fileBuffer);
+    const text = data.text;
+
+
+    const splitter = new RecursiveCharacterTextSplitter({});
+    const output = await splitter.createDocuments([text]);
+
+    await SupabaseVectorStore.fromDocuments(output, embeddings, {
+      client: client(),
+      tableName: 'documents',
+    })
+
+    res.status(200).json({ message: "PDF parsed", extractedText: text });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to parse PDF" });
+  }
 });
 
 app.post("/query", async (req, res) => {
@@ -47,40 +91,66 @@ app.post("/query", async (req, res) => {
     const documents = await new SimpleDirectoryReader().loadData({
       directoryPath: dataPath,
     });
-    const index = await VectorStoreIndex.fromDocuments(documents);
-    const queryEngine = index.asQueryEngine();
 
-    // Relevant nodes of context from document from querying the question to the engine
-    const receivedNodes = await queryEngine.retrieve({ query: question });
-    const contextText = receivedNodes
-      .map((item) => (item.node as TextNode).text)
-      .join("\n\n");
+    const text = await readFile("./data/abramov.txt", "utf-8");
 
-    // Prompt for the AI to follow when having a chat. Takes in user input, previous chat history, and contextText.
-    const chatPrompt = ChatPromptTemplate.fromMessages([
-      // chatTemplate
-      [
-        "system",
-        "you are an assistant who answers questions based on the document based on the context. \n context: {context}",
-      ],
-      new MessagesPlaceholder("chat_history"),
-      ["human", "{input}"],
-    ]);
+    const splitter = new RecursiveCharacterTextSplitter({});
+    // chunk the text
+    const output = await splitter.createDocuments([text]);
 
-    // Chain to invoke prompt and pass it to our llm. 
-    const chatChain = RunnableSequence.from([chatPrompt, chatModel]);
-    const response = await chatChain.invoke({
-      context: contextText,
-      input: question,
-      chat_history: await memory
-        .loadMemoryVariables({})
-        .then((v) => v.chat_history),
+    // send chunks and embed them into vector store
+    // const vectorStore = await SupabaseVectorStore.fromDocuments(
+    //   output,
+    //   new OpenAIEmbeddings(),
+    //   { client: client(), tableName: "documents", queryName: "match_documents" }
+    // );
+
+    // retrieiver to get info from the vectorstore
+    const vectorStore = new SupabaseVectorStore(embeddings, {
+      client: client(),
+      tableName: "documents",
+      queryName: "match_documents",
     });
 
-    // saves the response and human question from chat into memory
-    await memory.saveContext({ input: question }, { output: response.content });
+    const retriever = vectorStore.asRetriever();
 
-    res.json({ question: question, answer: response.content });
+    const result1 = await vectorStore.similaritySearch("Towns or cities");
+    console.log(result1);
+
+    // const index = await VectorStoreIndex.fromDocuments(documents);
+    // const queryEngine = index.asQueryEngine();
+
+    // Relevant nodes of context from document from querying the question to the engine
+    // const receivedNodes = await queryEngine.retrieve({ query: question });
+    // const contextText = receivedNodes
+    //   .map((item) => (item.node as TextNode).text)
+    //   .join("\n\n");
+
+    // Prompt for the AI to follow when having a chat. Takes in user input, previous chat history, and contextText.
+    // const chatPrompt = ChatPromptTemplate.fromMessages([
+    //   // chatTemplate
+    //   [
+    //     "system",
+    //     "you are an assistant who answers questions based on the document based on the context. \n context: {context}",
+    //   ],
+    //   new MessagesPlaceholder("chat_history"),
+    //   ["human", "{input}"],
+    // ]);
+
+    // // Chain to invoke prompt and pass it to our llm.
+    // const chatChain = RunnableSequence.from([chatPrompt, chatModel]);
+    // const response = await chatChain.invoke({
+    //   context: contextText,
+    //   input: question,
+    //   chat_history: await memory
+    //     .loadMemoryVariables({})
+    //     .then((v) => v.chat_history),
+    // });
+
+    // // saves the response and human question from chat into memory
+    // await memory.saveContext({ input: question }, { output: response.content });
+
+    // res.json({ question: question, answer: response.content });
   } catch (error) {
     console.error("Query error:", error);
     res.json({ error });
